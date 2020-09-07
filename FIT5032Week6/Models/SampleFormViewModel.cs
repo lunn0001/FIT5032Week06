bd15ab4742cebcf2b1e8d2171e4183afa6d136f2
Lakshmi Unnikrishnan <lunn0001@student.monash.edu>
2020-09-07 17:54:27 +0530

    Week 06 files

---
+++ FIT5032Week6/Models/SampleFormViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FIT5032Week6.Models
+{
+    public class SampleFormViewModel
+    {
+    }
+
+    public class FormOneViewModel
+    {
+        [Required]
+        [Display(Name ="First Name")]
+        public String FirstName { get; set; }
+
+        public String LastName { get; set; }
+    }
+}
